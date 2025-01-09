a31ae7885af6b8135a1d40f3c0e31462f153960b
MarcosAllysson <marcosgracip05@gmail.com>
2025-01-08 21:20:38 -0300

    feat: login page

---
+++ Login.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebFormsAgenda.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Text="Email"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Font-Bold="True" Text="Password"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="buttonLogin" runat="server" OnClick="buttonLogin_Click" Text="Login" />
+            <br />
+            <br />
+            <asp:Label ID="labelResponse" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebFormsAgenda
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            // Access database to check if user exists.
+            // Get string connection
+            System.Configuration.ConnectionStringSettings connectionString;
+
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            connectionString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+
+            // Create a connection object
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString.ToString();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+
+            // Using brakets because User is a keyword reserved by SQL Server.
+            command.CommandText = "SELECT * FROM [User] WHERE Email = @Email and Password = @Password;";
+            command.Parameters.AddWithValue("Email", email);
+            command.Parameters.AddWithValue("Password", password);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (!reader.HasRows)
+            {
+                labelResponse.Text = "Email or password are wrong!";
+            }
+            else
+            {
+                // direct to main page
+                labelResponse.Text = "It works!";
+            }
+        }
+    }
+}
